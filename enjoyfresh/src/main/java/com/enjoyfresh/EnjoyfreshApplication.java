96fff127a2fb2295a8f14cf6932a62b3a35884af
abhilash-cloudsolv <56075211+abhilash-cloudsolv@users.noreply.github.com>
2019-10-02 17:54:21 +0530

    test commit
    test

---
+++ enjoyfresh/src/main/java/com/enjoyfresh/EnjoyfreshApplication.java
@@ -0,0 +1,13 @@
+package com.enjoyfresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyfreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyfreshApplication.class, args);
+	}
+
+}
